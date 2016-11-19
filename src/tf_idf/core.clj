b8b6f15867f1a7aa344b297ad0a151140fc0176a
Colton McCurdy <mccurdyc@allegheny.edu>
2016-11-19 16:51:08 -0500

    updated core.clj to calculate term frequency per-file. broke functions up a bit more also to make testing easier.

--- src/tf_idf/core.clj
+++ src/tf_idf/core.clj
@@ -6,16 +6,30 @@
 ;; currently removes accented characters like: â, ê, etc.
 (def non-word-regex #"[^(? )\w]")
 
-(defn get-terms [f]
-  "Replace any non-word or non-whitespace characters with ''. Everything to lower-case. Split by ' ' character."
-    (st/split (st/lower-case (st/replace (slurp f) non-word-regex "")) #" "))
+(defn clean-file [f]
+  (st/lower-case (st/replace (slurp f) non-word-regex "")))
 
-(defn term-frequency [l]
-  (reduce (quot (frequencies l) (count l))))
+(defn get-terms-list [s]
+  (st/split s #" "))
+
+(defn calculate-tf [m v]
+ (float (/ v (count m))))
+
+(defn update-map [f coll]
+  (reduce-kv (fn [m k v] (assoc m k (f coll v))) (empty coll) coll))
+
+(defn calculate-tf-idf [f]
+  (let [file (clean-file f)
+        term-list (get-terms-list file)
+        term-counts (frequencies term-list)]
+        (update-map calculate-tf term-counts)))
+        ;; term-frequency (update-map calculate-tf term-counts)]))
 
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
   (println "Provide a directory:")
-  (let [files (file-seq (io/file (read-line)))
-        all-terms (doall (map get-terms (str files)))]))
+  (let [files (.listFiles (io/file (read-line)))
+        all-terms (doall (map get-terms files))
+        freqs (doall (map frequencies all-terms))]
+    (doall (map update-map calculate-tf freqs))))
