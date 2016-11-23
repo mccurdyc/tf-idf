527df28fd9051d1d07a971a1105c15bd2fc45ea2
Colton McCurdy <mccurdyc@allegheny.edu>
2016-11-23 16:46:54 -0500

    updated description, url, and license information

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
 (defproject tf-idf "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :description "A term-frequency inverse-document-frequency calculator"
+  :url "https://github.com/mccurdyc/tf-idf"
+  :license {:name "GNU General Public License v3.0"
+            :url "https://www.gnu.org/licenses/gpl-3.0.en.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]]
   :main ^:skip-aot tf-idf.core
   :target-path "target/%s"
