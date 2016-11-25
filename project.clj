7e31b84cb1df3f994ea108440e6d4ad3e510e776
Colton McCurdy <mccurdyc@allegheny.edu>
2016-11-25 15:46:04 -0500

    added clj-time to dependencies

--- project.clj
+++ project.clj
@@ -3,7 +3,8 @@
   :url "https://github.com/mccurdyc/tf-idf"
   :license {:name "GNU General Public License v3.0"
             :url "https://www.gnu.org/licenses/gpl-3.0.en.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]]
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [clj-time "0.12.2"]]
   :main ^:skip-aot tf-idf.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
