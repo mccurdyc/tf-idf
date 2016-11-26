f757f3f5326d7381e7bd0d0b11146a3b77f32079
Colton McCurdy <mccurdyc@allegheny.edu>
2016-11-26 16:29:55 -0500

    added dependency for timing

--- project.clj
+++ project.clj
@@ -4,7 +4,8 @@
   :license {:name "GNU General Public License v3.0"
             :url "https://www.gnu.org/licenses/gpl-3.0.en.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [clj-time "0.12.2"]]
+                 [clj-time "0.12.2"]
+                 [org.clojure/tools.cli "0.3.5"]]
   :main ^:skip-aot tf-idf.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
